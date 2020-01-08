9e533e0dfd96356919b70b14bc3f5e26f9660015
cf.meng@gmail.com <cf.meng@gmail.com>
2020-01-07 20:11:55 -0500

    Cleanup

--- src/tool/fd_sort.py
+++ src/tool/fd_sort.py
@@ -10,7 +10,6 @@ def save_dict_to_hdf5(dic, filename):
     """
     with h5py.File(filename, 'w') as h5file:
         recursively_save_dict_contents_to_group(h5file, '/', dic)
-
 def recursively_save_dict_contents_to_group(h5file, path, dic):
     """
     ....
@@ -22,25 +21,6 @@ def recursively_save_dict_contents_to_group(h5file, path, dic):
             recursively_save_dict_contents_to_group(h5file, path + key + '/', item)
         else:
             raise ValueError('Cannot save %s type'%type(item))
-
-def load_dict_from_hdf5(filename):
-    """
-    ....
-    """
-    with h5py.File(filename, 'r') as h5file:
-        return recursively_load_dict_contents_from_group(h5file, '/')
-
-def recursively_load_dict_contents_from_group(h5file, path):
-    """
-    ....
-    """
-    ans = {}
-    for key, item in h5file[path].items():
-        if isinstance(item, h5py._hl.dataset.Dataset):
-            ans[key] = item.value
-        elif isinstance(item, h5py._hl.group.Group):
-            ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/')
-    return ans
 
 name_sol = sys.argv[1]
 files_seis=glb.glob(name_sol+"_*_ofd.txt")
